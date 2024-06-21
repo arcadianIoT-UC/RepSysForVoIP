b762df61c201fa00eac35f9369839685cde55f0b
BAFCruz <94404248+BAFCruz@users.noreply.github.com>
2024-06-21 11:08:26 +0100

    Update ObjScore.java - Fixed USERREPUTATION

--- src/main/java/eu/tutorial/objects/ObjScore.java
+++ src/main/java/eu/tutorial/objects/ObjScore.java
@@ -1,41 +1,65 @@
 package eu.tutorial.objects;
 
 public class ObjScore {
-    private int id ;
+    private int id;
     private String IP;
-    private double score ;
+    private String SRC;
+    private double score;
 
-    public ObjScore(int i, String iIP, double iScore){
-        id= i;
-        IP = iIP;
+    private ObjScore(int i, String ip, String src, double iScore) {
+        id = i;
+        IP = ip;
+        SRC = src;
         score = iScore;
     }
 
-    public String toString(){
-        return "id="+id+ " IP="+ IP + " score=" +score;
+    // IP-based objects
+    public static ObjScore createWithIP(int id, String ip, double score) {
+        return new ObjScore(id, ip, null, score);
     }
 
+    // SRC-based objects
+    public static ObjScore createWithSRC(int id, String src, double score) {
+        return new ObjScore(id, null, src, score);
+    }
 
-    public int getId(){
+    @Override
+    public String toString() {
+        return "id=" + id + 
+               (IP != null ? " IP=" + IP : "") + 
+               (SRC != null ? " SRC=" + SRC : "") + 
+               " score=" + score;
+    }
+
+    public int getId() {
         return this.id;
     }
-    public String getIP(){
+
+    public String getIP() {
         return this.IP;
     }
-    public double getScore(){
+
+    public String getSRC() {
+        return this.SRC;
+    }
+
+    public double getScore() {
         return this.score;
     }
 
-    public void setId(int iId){
-        this.id=iId;
-    }
-    public void setIP(String iIP){
-        this.IP=iIP;
-    }
-    public void setScore(double iS){
-        this.score=iS;
+    public void setId(int iId) {
+        this.id = iId;
     }
 
+    public void setIP(String iIP) {
+        this.IP = iIP;
+    }
 
+    public void setSRC(String iSRC) {
+        this.SRC = iSRC;
+    }
 
+    public void setScore(double iS) {
+        this.score = iS;
+    }
 }
