0a562f51cfe344f42732b6a84b7c4fee8f345dc2
BAFCruz <94404248+BAFCruz@users.noreply.github.com>
2024-06-21 11:10:24 +0100

    Update MyDB.java - Fixed USERREPUTATION

--- src/main/java/eu/tutorial/components/MyDB.java
+++ src/main/java/eu/tutorial/components/MyDB.java
@@ -8,9 +8,6 @@ import java.sql.SQLException;
 
 import eu.tutorial.objects.ObjScore;
 
-/*
-Class to interact with the DB
-*/
 public class MyDB {
     private String dbURL;
     private String dbDB;
@@ -19,112 +16,84 @@ public class MyDB {
     private Connection connection;
 
     public MyDB(String idbRUL, String iDB, String iUser, String iPass ){
-        // Some treatment
         dbURL = idbRUL;
-        dbDB=iDB;
+        dbDB = iDB;
         dbUSER = iUser;
         dbPASSWORD = iPass;
-        
+
         System.out.println("Connecting to database " + dbURL + " " + dbUSER);
 
-        try{
+        try {
             connection = DriverManager.getConnection(dbURL, dbUSER, dbPASSWORD);
             System.out.println("Connected to database " + dbURL + " " + dbUSER);
-        }catch(Exception e){
-            //TODO
+        } catch(Exception e) {
             e.printStackTrace();
         }
     }
 
-    public ObjScore getObjScore(String ipAddr){
-        
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-
-        try{
-            // Create a simple query
-            String sql = "SELECT * FROM ip_score_test  where IP_address = '" + ipAddr + "'";
-
-            // Create a statement
-            preparedStatement = connection.prepareStatement(sql);
-            resultSet = preparedStatement.executeQuery();
-            
-            while (resultSet.next()) {
-                // Retrieve data by column name
-                int id = resultSet.getInt("id");
-                String IP = resultSet.getString("IP_address");
-                double score = resultSet.getDouble("score");
-                if (ipAddr.compareToIgnoreCase(IP)==0){
-                    return new ObjScore(id, IP, score);
-                }
-            }
-        }catch(Exception e){
-            //TODO:
-        }finally{
-            try{
-                resultSet.close();
-                preparedStatement.close();
-            }catch(Exception e){
-                //TODO:
-            }
-        }
-
-        return null;
+    public ObjScore getObjScore(String ipAddr) {
+        return null; 
     }
 
-    
-    public boolean addUpdateIPScore(String ip, double iS){
-        boolean res=false;
+    public boolean addUpdateIPScore(String ip, double iS) {
+        boolean res = false;
         PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
 
-        try{
-            // Create a simple query
-            String sql = "INSERT INTO ip_score (IP_address, score) VALUES('" + ip+ "'," + iS +") ON DUPLICATE key UPDATE score = " + iS ;
-
+        try {
+            
+            String sql = "INSERT INTO ip_score (IP_address, score) VALUES('" + ip + "'," + iS + ") ON DUPLICATE key UPDATE score = " + iS;
             System.out.println("To EXEC  " + sql );
-            // Create a statement
+          
             preparedStatement = connection.prepareStatement(sql);
-            int nRows  = preparedStatement.executeUpdate();
+            int nRows = preparedStatement.executeUpdate();
             System.out.println("Executed " + sql + " " + nRows);
             preparedStatement.close();
 
-            if (nRows == 1){res= true;}
-            else res= false;
-            
-        }catch(Exception e){
-            //TODO:
+            if (nRows == 1) { res = true; }
+            else { res = false; }
+
+            ObjScore objScore = ObjScore.createWithIP(getIdForIP(ip), ip, iS);
+           
+
+        } catch(Exception e) {
             System.out.println("Error on insert");
             e.printStackTrace();
         }        
         return res;
     }
 
-    public boolean addUpdateUSERScore(String ip, double iS){
-        boolean res=false;
+    public boolean addUpdateUSERScore(String src, double iS) {
+        boolean res = false;
         PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
 
-        try{
-            // Create a simple query
-            String sql = "INSERT INTO user_score (user, score) VALUES('" + ip+ "'," + iS +") ON DUPLICATE key UPDATE score = " + iS ;
-
+        try {
+            
+            String sql = "INSERT INTO user_score (user, score) VALUES('" + src + "'," + iS + ") ON DUPLICATE key UPDATE score = " + iS;
             System.out.println("To EXEC  " + sql );
-            // Create a statement
+            
             preparedStatement = connection.prepareStatement(sql);
-            int nRows  = preparedStatement.executeUpdate();
+            int nRows = preparedStatement.executeUpdate();
             System.out.println("Executed " + sql + " " + nRows);
             preparedStatement.close();
 
-            if (nRows == 1){res= true;}
-            else res= false;
+            if (nRows == 1) { res = true; }
+            else { res = false; }
+
+            ObjScore objScore = ObjScore.createWithSRC(getIdForSRC(src), src, iS);
             
-        }catch(Exception e){
-            //TODO:
+
+        } catch(Exception e) {
             System.out.println("Error on insert");
             e.printStackTrace();
         }        
         return res;
+    }
+
+    private int getIdForIP(String ip) {
+        return 1; 
+    }
+
+    private int getIdForSRC(String src) {
+        return 2;
     }
 }
-
